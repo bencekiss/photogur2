0b5f93b8fcecfd9283ba8486b7cd9dc4eb14aa72
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-28 21:10:55 -0500

    Create route, controller and view for http://localhost:3000/images

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'images/index'
+
+  get 'images/new'
+
+  get 'images/create'
+
+  get 'images/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'images' => 'images#index'
+
 end
