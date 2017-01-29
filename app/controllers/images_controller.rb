12abed9525f77ee542b80cc5424d655dd937fac3
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-28 22:10:00 -0500

    Adding feature to save new picture and refactoring to save it to the database

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,12 +4,24 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to images_url
+    else
+      render :new
+    end
   end
 
   def show
     @image = Image.find(params[:id])
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:title, :artist, :url, :year)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'images' => 'images#index'
+
+  post 'images' => 'images#create'
+  get 'images/new' => 'images#new'
+  
   get 'images/:id' => 'images#show', as: 'image'
+
+
 
 end
