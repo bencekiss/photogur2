853ed35210185837051280fc04110f334303725a
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-28 21:38:06 -0500

    Making images show at /images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Image.create(
+  title: 'The old church at the cost of the white sea',
+  artist: 'Sergey Ershov',
+  url: 'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg',
+  year: 2003
+)
+Image.create(
+  title: 'Sea Power',
+  artist: 'Stephen Scullion',
+  url: 'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg',
+  year: 2007
+)
+Image.create(
+  title: 'Into the Poppies',
+  artist: 'John Wilhelm',
+  url: 'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg',
+  year: 2000
+)
